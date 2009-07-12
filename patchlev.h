dbedc2594e41c8083151f3e67c76b55049e0bbcf
Thomas E. Dickey <dickey@invisible-island.net>
2009-07-12 21:34:21 +0000

    update patch-date (it's not November 1996 anymore)

--- patchlev.h
+++ patchlev.h
@@ -1,5 +1,7 @@
-/* mawk 1.3 */
+/*
+ * $MawkId$
+ */
 #define  PATCHLEVEL	3
 #define  PATCH_STRING	".3"
-#define  DATE_STRING    "Nov 1996"
+#define  DATE_STRING    "20090712"
 #define  MAWK_ID	"@(#)mawk 1.3.3"
