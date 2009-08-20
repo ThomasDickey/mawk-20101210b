d19e731eb5a258e95d1ff92e8f6bac51d24fc740
Thomas E. Dickey <dickey@invisible-island.net>
2009-08-20 23:13:50 +0000

    fix...

--- patchlev.h
+++ patchlev.h
@@ -1,6 +1,6 @@
 /*
- * $MawkId: patchlev.h,v 1.6 2009/07/28 23:30:28 tom Exp $
+ * $MawkId: patchlev.h,v 1.7 2009/08/01 15:26:15 tom Exp $
  */
 #define  PATCHLEVEL	3
 #define  PATCH_STRING	".3"
-#define  DATE_STRING    "20090801"
+#define  DATE_STRING    "20090820"
