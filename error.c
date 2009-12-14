5b0d5eb78cf18d3910336ab2afedb16d25dfc0ad
Thomas E. Dickey <dickey@invisible-island.net>
2009-12-14 00:36:47 +0000

    missing() is assuming 'ln' is unsigned - and it was - but param said int.

--- error.c
+++ error.c
@@ -10,7 +10,7 @@ the GNU General Public License, version 2, 1991.
 ********************************************/
 
 /*
- * $MawkId: error.c,v 1.8 2009/07/27 15:26:36 tom Exp $
+ * $MawkId: error.c,v 1.9 2009/07/28 23:23:22 tom Exp $
  * @Log: error.c,v @
  * Revision 1.6  1995/06/06  00:18:22  mike
  * change mawk_exit(1) to mawk_exit(2)
@@ -124,7 +124,7 @@ static int missing_rbrace[] =
 {EOF, BEGIN, END, 0};
 
 static void
-missing(int c, const char *n, int ln)
+missing(int c, const char *n, unsigned ln)
 {
     const char *s0, *s1;
 
