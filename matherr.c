de2335dc6df734ef443650d8ae4d40e5fd3ca1c6
Thomas E. Dickey <dickey@invisible-island.net>
2010-02-21 15:37:21 +0000

    fix for Visual Studio 6

--- matherr.c
+++ matherr.c
@@ -10,7 +10,8 @@ the GNU General Public License, version 2, 1991.
 ********************************************/
 
 /*
- * $MawkId: matherr.c,v 1.21 2009/12/16 01:34:23 tom Exp $
+ * $MawkId: matherr.c,v 1.22 2009/12/18 10:22:03 tom Exp $
+ *
  * @Log: matherr.c,v @
  * Revision 1.9  1996/09/01 16:54:35  mike
  * Third try at bug fix for solaris strtod.
