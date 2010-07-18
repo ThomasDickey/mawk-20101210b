a1c8c1903abc7f360d464faec2ed687e1abb51ef
Thomas E. Dickey <dickey@invisible-island.net>
2010-07-18 10:30:07 +0000

    Date: Fri, 9 Jul 2010 15:47:37 +0200 (CEST) From: Heiner Marxen <Heiner.Marxen@DrB.Insel.DE> To: dickey@his.com Subject: Hint for mawk 1.3.4 20100625 X-Mailer: ELM [version 2.5 PL5]
    Hello Thomas,
    
    I just wanted to let you know about some suspect code, which may or may not
    be a bug.
    
    In file "scan.c", lines 450..452 I read:
        case SC_MATCH:
            string_buff[0] = '~';
            string_buff[0] = 0;
    
    That second [0] obviously should be [1], shouldn't it?
    -------TD: looks like the string_buff is used only in error-reporting, e.g.,
    to show the token value.  It's old and was un-noticed.

--- scan.c
+++ scan.c
@@ -10,7 +10,7 @@ the GNU General Public License, version 2, 1991.
 ********************************************/
 
 /*
- * $MawkId: scan.c,v 1.21 2010/05/07 22:03:19 tom Exp $
+ * $MawkId: scan.c,v 1.22 2010/06/25 22:34:36 tom Exp $
  * @Log: scan.c,v @
  * Revision 1.8  1996/07/28 21:47:05  mike
  * gnuish patch
@@ -448,7 +448,7 @@ yylex(void)
 	ct_ret(RBOX);
 
     case SC_MATCH:
-	string_buff[0] = '~';
+	string_buff[1] = '~';
 	string_buff[0] = 0;
 	yylval.ival = 1;
 	ct_ret(MATCH);
